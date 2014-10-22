6ec4820e1ec84b0540141870d7c6b92a8959417b
“Melissa” <melissa@melissajclark.ca>
2014-10-22 13:54:46 -0400

    used preventdefault so form doesn't refresh

---
+++ alibi-jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+$("form.questions").on("submit",function(event){
+	event.preventDefault(); // prevents form from refreshing
+	}); // end of form.questions event function
+
+
+
+
+
+
+
+
+}); // end doc ready function
+
