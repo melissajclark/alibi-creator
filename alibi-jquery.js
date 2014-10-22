8a0c1ad8a370d73a699a9f1301850b1a8780604f
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-22 18:52:55 -0400

    made submit wobble

--- alibi-jquery.js
+++ alibi-jquery.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 
 
-$("form.questions").on("submit",function(event){
-	event.preventDefault(); // prevents form from refreshing
-}); // end of form.questions event function
+	$("form.questions").on("submit",function(event){
+		event.preventDefault(); // prevents form from refreshing
+	}); // end of form.questions event function
 	
+	$("input[type=submit]").on("click",function(){
+		$(this).addClass("animated wobble");
 
+	}); // end of click submit function
 
 }); // end doc ready function
 
