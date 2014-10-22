961d5e0c0b76e26d6a59f94f0d0f3afa74111473
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-22 18:46:51 -0400

    no major change

--- alibi-jquery.js
+++ alibi-jquery.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 
 $("form.questions").on("submit",function(event){
 	event.preventDefault(); // prevents form from refreshing
-	}); // end of form.questions event function
+}); // end of form.questions event function
+	
 
 
 }); // end doc ready function
