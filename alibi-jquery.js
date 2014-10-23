01618df0d55fd2692f507687d7430c15b095432d
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-22 20:09:09 -0400

    removed notes

--- alibi-jquery.js
+++ alibi-jquery.js
@@ -36,17 +36,4 @@ $(document).ready(function(){
 }); // end doc ready function
 
 
-// Notes:
-
-// $("[value~='mom']")
-// selects elements with the value attribute and the word mom
-
-
-// http://learn.jquery.com/using-jquery-core/selecting-elements/
-// :checked
-
-// Not to be confused with :checkbox, :checked targets checked checkboxes, but keep in mind that this selector works also for checked radio buttons, and select elements (for select elements only, use the :selected selector):
-
-// 1
-// $( "form :checked" );
 
