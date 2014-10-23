629bce45ef1780f5131b14966c2436333ec08387
Melissa Jean Clark <melissa@melissajclark.ca>
2014-10-22 21:15:44 -0400

    put scaleofIssueQ inside submit event function, changed whosePissed to alibiQuestion.who

--- alibi-jquery.js
+++ alibi-jquery.js
@@ -18,10 +18,10 @@ $(document).ready(function(){
 		// more alibi questions?
 	}
 
-	// this function defines whosePissed alibi is for & stores the value
-	$("fieldset.whosePissed input[type='radio']").on("change", function(){
-		 whosePissed = $(this).val(); 
-	}); // end of function listening on "whosePissed" question
+	// this function defines alibiQuestion.who alibi is for & stores the value
+	$("fieldset.alibiQuestion.who input[type='radio']").on("change", function(){
+		 alibiQuestion.who = $(this).val(); 
+	}); // end of function listening on "alibiQuestion.who" question
 
 	// this function defines whether it's the first time or not & stores the value
 	$("fieldset.alibiTiming input[type='radio']").on("change", function(){
@@ -37,18 +37,38 @@ $(document).ready(function(){
 	$("input[type=submit]").on("click",function(){
 		$(this).addClass("animated wobble"); // fun effect for submit
 
-		if (whosePissed == "other") { // checks the value of input with the value of "other"
-			whosePissed = $("fieldset.whosePissed input[type=text]").val();
+		if (alibiQuestion.who == "other") { // checks the value of input with the value of "other"
+			alibiQuestion.who = $("fieldset.alibiQuestion.who input[type=text]").val();
 		}
 
-		console.log("whosePissed alibi is for: " + whosePissed + ", " + "Happened before? "+ firstTimeAnswer + ", Scale of mess up: " + scaleOfIssueQ); // logs user's answers
+		console.log("alibiQuestion.who alibi is for: " + alibiQuestion.who + ", " + "Happened before? "+ firstTimeAnswer + ", Scale of mess up: " + scaleOfIssueQ); // logs user's answers
+
+		/**
+		*
+		* Code Section 2: Alibi Summary for User
+		*
+		**/
+
+		// this section analyzes user's answer to scaleofIssue prompt
+		var scaleOfIssueQ = ""; 
+
+		if (alibiQuestion.scaleOfIssue === 10 || alibiQuestion.scaleOfIssue >= 10) {
+			scaleOfIssueQ = "Holy shit, this could be bad. I don't know if I can get you outta this one";
+		} else if (alibiQuestion.scaleOfIssue >= 8 && alibiQuestion.scaleOfIssue <= 9) {
+			scaleOfIssueQ = "Eep, that bad's pretty bad. I'll do my best";
+		} else if (alibiQuestion.scaleOfIssue >= 6 && alibiQuestion.scaleOfIssue <= 7 ) {
+			scaleOfIssueQ = "Hmmm, alright, I think I can help you out";
+		} else if (alibiQuestion.scaleOfIssue >= 4 && alibiQuestion.scaleOfIssue <= 5) {
+			scaleOfIssueQ = "Okay, sounds like didn't mess up too badly, I got your back. We'll get through this together";
+		} else if (alibiQuestion.scaleOfIssue >= 2 && alibiQuestion.scaleOfIssue <= 3) {
+			scaleOfIssueQ = "You barely messed up! Let's call this a misdemeanour";
+		} else if (alibiQuestion.scaleOfIssue = 1) {
+			scaleOfIssueQ = "You know what, you'll be fine. I bet your " + alibiQuestion.who + " is already over it. Let's get you an alibi, just in case" 
+		}
+		console.log(scaleOfIssueQ);
+
 	}); // end of click submit function
 
-/**
-*
-* Code Section 2: Alibi Summary for User
-*
-**/
 
 
 	 
