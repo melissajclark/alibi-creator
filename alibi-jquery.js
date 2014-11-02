5574117c0d516f2d85fdc8e24e58b8d2726e7be6
Melissa Jean Clark <melissa@melissajclark.ca>
2014-11-02 17:02:22 -0500

    removed commented out code

--- alibi-jquery.js
+++ alibi-jquery.js
@@ -141,12 +141,6 @@ $(document).ready(function(){
 	var alibiOption2 = "Believe me, I wish I could have been there. " + prefix[1] + " " + random(alibiOption2) + " and it was " + random(badWord) + "! " + random(moreAlibi) + " " + random(alibiApology);
 	var alibiOption3 = "I can't believe I'm saying this aloud, I feel  " + random(badWord) + ". But, I need to be honest with you... " + prefix[2] + " " + random(alibiOption3) + ". " + random(moreAlibi) + " " + random(alibiApology);
 
-	// defines function for creating alibi optiopns	 
-	// var createAlibi = function(){	
-	// 	console.log("Okay, here's three options for you to tell your " + alibiQuestion.who + ". " + "1. " + alibiOption1 + "2. " + alibiOption2 + "3. " + alibiOption3);
-	// 	return "Okay, here's three options for you to tell your " + alibiQuestion.who + ". " + "1. " + alibiOption1 + "." + "2. " + alibiOption2 + "3. " + alibiOption3 + ".";
-	// };
-	// createAlibi(); // calls function
 
 /**
 *
@@ -162,6 +156,7 @@ $(document).ready(function(){
 		$("section.alibiOutput ol").append("<li>"+alibiOption3+"</li>");
 
 	});
+	
 
 }); // end doc ready function
 
