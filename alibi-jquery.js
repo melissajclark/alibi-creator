26ba9b01547c1f66279184932004b0fa59fdee89
Melissa Jean Clark <melissa@melissajclark.ca>
2014-11-02 14:15:17 -0500

    not working still

--- alibi-jquery.js
+++ alibi-jquery.js
@@ -23,21 +23,22 @@ $(document).ready(function(){
 		// more alibi questions?
 	}
 
+	var nameofPissed = "";
+
 	// this function defines alibiQuestion.who alibi is for & stores the value
 	$("fieldset.who input[type='radio']").on("change", function(){
 		 alibiQuestion.who = $(this).val(); 
 
-	var nameQhtml = '<label for="who_9">' + 'Whats your ' + alibiQuestion.who + '&#39;s ' + 'name?' + '</label><span class="whoName"><br/><input type="text" name="their" id="who_8" placeholder="name" /></span>';
+		var nameQhtml = '<label for="who_9">' + 'Whats your ' + alibiQuestion.who + '&#39;s ' + 'name?' + '</label><span class="whoName"><br/><input type="text" name="their" id="who_8" placeholder="name" /></span>';
 
 		 if (alibiQuestion.who != "other" ) { // checks the value of input with the value of "other"
 		 	$(this).parent().after("<p>"+nameQhtml+"</p>"); // this selects the parent of the selector and adds the html after it
 		 	// console.log(nameQhtml);
 		 }
 
-	var nameofPissed = $("fieldset.who input[type=text]").val();
+		nameofPissed = $("fieldset.who input[type=text]").val();
+		// console.log(alibiQuestion.nameofPissed);
 	}); // end of function listening on "alibiQuestion.who" question
-
-
 
 	// this function defines whether it's the first time or not & stores the value
 	$("fieldset.alibiTiming input[type='radio']").on("change", function(){
