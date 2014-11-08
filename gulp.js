5b3a074093d551e94651cf55895d3849f7dd456d
“Melissa” <melissa@melissajclark.ca>
2014-11-07 20:22:17 -0500

    removed gulp stuff, gonna use grunt

--- gulp.js
+++
@@ -1,9 +0,0 @@
-var gulp      = require('gulp'),
-    takana    = require('takana');
- 
-gulp.task('takana', function() {
-  takana.run({
-    path:         'assets/',
-    includePaths: [] // Optional
-  });
-});
--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp      = require('gulp'),
-    takana    = require('takana');
- 
-gulp.task('takana', function() {
-  takana.run({
-    path:         __assets,
-    includePaths: [] // Optional
-  });
-});
