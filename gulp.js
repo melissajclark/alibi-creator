f3b663e8ce061b35ac963a50480870d0f24d7767
“Melissa” <melissa@melissajclark.ca>
2014-11-07 20:12:10 -0500

    trying gulp for use with takana

---
+++ gulp.js
@@ -0,0 +1,9 @@
+var gulp      = require('gulp'),
+    takana    = require('takana');
+ 
+gulp.task('takana', function() {
+  takana.run({
+    path:         'assets/',
+    includePaths: [] // Optional
+  });
+});
