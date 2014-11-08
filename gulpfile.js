8f3541e85ad61464c2be555697f8149ddaf9715f
“Melissa” <melissa@melissajclark.ca>
2014-11-07 20:51:12 -0500

    hmmm

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
+gulp.task('default', ['takana']);
+
 var gulp      = require('gulp'),
     takana    = require('takana');
  
 gulp.task('takana', function() {
   takana.run({
-    path:         assets,
-    includePaths: [] // Optional
+    path:    (['/assets/'])
+    // includePaths: [] // Optional
   });
 });
